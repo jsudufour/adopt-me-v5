56a1186ad493cefabd1ff62e0736a4eb21e072a7
Josiane Dufour <josiane@Josianes-MBP.lan>
2019-10-17 17:12:08 +0100

    integrate ErrorBoundary component into Details component by exporting errorBoundary component with default Details component export

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends React.Component {
   // constructor(props) {
@@ -16,6 +17,7 @@ class Details extends React.Component {
   state = { loading: true };
 
   componentDidMount() {
+    throw new Error("lol");
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
         name: animal.name,
@@ -50,4 +52,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
