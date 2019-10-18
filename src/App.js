61f696d3191d2f2e7ca445829a54a1517ec5075a
Josiane Dufour <josiane@Josianes-MBP.lan>
2019-10-18 14:43:45 +0100

    add value to themeHook as an example

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const themeHook = useState("darkblue");
+  const themeHook = useState({
+    buttonColor: "darkblue",
+    modalColor: "pink"
+  });
   return (
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
